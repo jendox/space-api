e89f96287a5bc42c516bf8dd1dd052b05b3d2989
jendox <jendox1985@gmail.com>
2025-07-30 21:25:37 +0300

    added route /counter

--- api.py
+++ api.py
@@ -1,8 +1,10 @@
+import asyncio
 import random
+from itertools import count
 from pathlib import Path
 
 from fastapi import APIRouter
-from starlette.responses import HTMLResponse
+from starlette.responses import HTMLResponse, StreamingResponse
 
 space_facts = [
     {"id": 1, "fact": "На Сатурне и Юпитере идут дожди из алмазов"},
@@ -38,6 +40,23 @@ async def main_page():
 )
 async def get_info():
     return company_info
+
+
+@router.get(
+    path="/counter",
+    description="Get endless counter"
+)
+async def get_counter():
+    async def counter_generator():
+        counter = count(start=1, step=1)
+        while True:
+            await asyncio.sleep(0.5)
+            yield f"{next(counter)} "
+
+    return StreamingResponse(
+        counter_generator(),
+        media_type="text/html",
+    )
 
 
 @router.get(

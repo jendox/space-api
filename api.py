9869726e2691a71fede2724c9c1a3eb6dca3c3f6
jendox <jendox1985@gmail.com>
2025-08-03 22:13:00 +0300

    added .editorconfig & ruff.toml

--- api.py
+++ api.py
@@ -1,18 +1,9 @@
 import asyncio
-import random
 from itertools import count
 from pathlib import Path
 
 from fastapi import APIRouter
 from starlette.responses import HTMLResponse, StreamingResponse
-
-space_facts = [
-    {"id": 1, "fact": "На Сатурне и Юпитере идут дожди из алмазов"},
-    {"id": 2, "fact": "Солнечному свету требуется 8 минут 20 секунд, чтобы достичь Земли"},
-    {"id": 3, "fact": "В космосе тьма, потому что нет атмосферы, которая рассеивает свет"},
-    {"id": 4, "fact": "МКС - самый дорогой объект, когда-либо построенный человечеством"},
-    {"id": 5, "fact": "Венера - самая горячая планета в Солнечной системе"},
-]
 
 company_info = {"company_name": "Horns and Hooves", "company_address": "Pushkin street, 12"}
 
@@ -21,8 +12,11 @@ router = APIRouter()
 
 def read_html_file():
     path = Path("templates/index.html")
-    with open(path, "r") as file:
-        return file.read()
+    with open(path, "r", encoding="utf-8") as file:
+        content = file.read()
+        return content.replace("{{company_name}}", company_info["company_name"]).replace(
+            "{{company_address}}", company_info["company_address"]
+        )
 
 
 @router.get(
@@ -34,42 +28,20 @@ async def main_page():
     return read_html_file()
 
 
-@router.get(
-    path="/info",
-    description="Get company info"
-)
+@router.get(path="/info", description="Get company info")
 async def get_info():
     return company_info
 
 
-@router.get(
-    path="/counter",
-    description="Get endless counter"
-)
+@router.get(path="/counter", description="Get endless counter")
 async def get_counter():
     async def counter_generator():
         counter = count(start=1, step=1)
         while True:
             await asyncio.sleep(0.5)
-            yield f"{next(counter)} "
+            yield f"{counter.__next__()} "
 
     return StreamingResponse(
         counter_generator(),
         media_type="text/html",
     )
-
-
-@router.get(
-    path="/random-fact",
-    description="Get one random fact about space"
-)
-async def get_random_fact():
-    return random.choice(space_facts)
-
-
-@router.get(
-    path="/all-facts",
-    description="Get all facts about space"
-)
-async def get_all_facts():
-    return {"facts": space_facts, "count": len(space_facts), "status": "success"}
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 from api import router
 
-app = FastAPI(title="SpaceAPI", version="0.1", description="Find out interesting facts about space")
+app = FastAPI(title="Horn&Hooves API", version="0.1",
+              description="Find out interesting facts about space")
 
 app.include_router(router=router)
